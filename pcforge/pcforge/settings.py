2b7552e19a479aa6db87cafbaff1443c170fcbdf
BobbyP-bit <rpicasiojr@gmail.com>
2025-02-26 23:00:37 -0600

    Add files via upload

--- pcforge/pcforge/settings.py
+++ pcforge/pcforge/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'pcforge.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'pcforge',
-	'USER': 'pcforge',
-	'PASSWORD':'pcforge2025',
-	'HOST': 'localhost',
-	'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
